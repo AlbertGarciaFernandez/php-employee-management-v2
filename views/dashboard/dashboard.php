24c91ed7bbce83202e92e4cc6ca23abc30b6bd0c
Jose Rodriguez <jose123135@gmail.com>
2022-01-28 15:47:19 +0100

    Dashaboard changes

--- views/dashboard/dashboard.php
+++ views/dashboard/dashboard.php
@@ -14,7 +14,7 @@ require ASSETS . "html/header.php";
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
 <script src="./../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
-<link rel="stylesheet" href="../assets/css/main.css">
+<link rel="stylesheet" href="<?=ASSETS?>css/main.css">
 <script src="../assets/js/index.js"></script>
 <!-- todo when the page load call function to fill the table -->
 <body onload=" callGrid(); ">
