5598675ea2ebc27c1feb7379e3316136ea941a8f
Jose Rodriguez <jose123135@gmail.com>
2022-01-28 16:43:13 +0100

    Added another sql request

--- views/dashboard/dashboard.php
+++ views/dashboard/dashboard.php
@@ -5,19 +5,11 @@ require ASSETS . "html/header.php";
 
 <!-- todo Import here because if we import in header.html it doesn't work-->
 <!-- todo import from CDN and Others -->
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Luxurious+Roman&display=swap" rel="stylesheet">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
-    integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA=="
-    crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
-<script src="./../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+
 <link rel="stylesheet" href="<?=ASSETS?>css/main.css">
 <script src="../assets/js/index.js"></script>
 <!-- todo when the page load call function to fill the table -->
-<body onload=" callGrid(); ">
+<body>
     <?php
         require ASSETS . "html/navbar.php";
     ?>
