efd04609d5b55445257a3ea0d96d367109f7b907
Albert Garcia Fernandez <alberd_beto@hotmail.com>
2022-01-31 14:01:26 +0100

    fix bug

--- models/employeeFormModel.php
+++ models/employeeFormModel.php
@@ -35,7 +35,7 @@ return null;
             $query = $this->db->connect()->prepare(
                 "INSERT INTO employees (name, lasName, email, gender, streetAdress, city, state, postalCode, phoneNumber) VALUES (:name, :lasName, :email, :gender, :streetAdress, :city, :state, :postalCode, :phoneNumber)"
             );
-            $query->execute(["name" => $data["name"], "lasName" => $data["lasName"], "email" => $data["email"], ["gender" => $data["gender"], "streetAdress" => $data["streetAdress"], "city" => $data["city"], ["state" => $data["state"], "postalCode" => $data["postalCode"], "phoneNumber" => $data["phoneNumber"]]);
+            $query->execute(["name" => $data["name"], "lasName" => $data["lasName"], "email" => $data["email"], "gender" => $data["gender"], "streetAdress" => $data["streetAdress"], "city" => $data["city"], "state" => $data["state"], "postalCode" => $data["postalCode"], "phoneNumber" => $data["phoneNumber"]]);
             return true;
         } catch (PDOException $e) {
             return false;
